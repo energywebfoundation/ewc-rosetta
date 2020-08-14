2d29cc4fd5b62532608d7461bd685b43ec18e5cb
Mani Hagh Sefat <mani.hagh.sefat@energyweb.org>
2020-08-14 21:38:01 +0800

    Add swagger UI initialization and env config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('ewc-rosetta')
+    .setDescription('Energy Web Chain Rosetta API description')
+    .setVersion(process.env.API_SEPC_VERVION)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
