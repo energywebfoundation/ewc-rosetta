43933411b68d62664260a424068d0cdbc869a3df
Piotr Kosiński <piotrkosinski@gmail.com>
2020-08-18 10:47:23 +0200

    chore(): remove sample code. change port to 8080 (default rosetta verification port)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from "@nestjs/common";
-import { AppService } from "./app.service";
 import { BlockModule } from "./block/block.module";
 
 @Module({
   imports: [BlockModule],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ require("dotenv").config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
