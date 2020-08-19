679383b6d3b7e42de6ba7d6e4f814d433ce936ff
Mani Hagh Sefat <mani.hagh.sefat@energyweb.org>
2020-08-19 23:32:49 +0800

    Implement mempool endpoint methods

--- src/mempool/mempool.module.ts
+++ src/mempool/mempool.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
 import { MempoolController } from "./mempool.controller";
 import { MempoolService } from "./mempool.service";
 
 @Module({
   controllers: [MempoolController],
   providers: [MempoolService],
+  imports: [
+    ScheduleModule.forRoot()
+  ]
+  
 })
 export class MempoolModule {}
--- src/mempool/mempool.service.ts
+++ src/mempool/mempool.service.ts
@@ -1,25 +1,54 @@
 import { Injectable } from "@nestjs/common";
+import { Cron, CronExpression } from "@nestjs/schedule";
 import { getRPCProvider } from "../utils/client";
+import { TransactionIdentifier } from "../models/TransactionIdentifer";
+import { OperationFactory } from "../models/OperationFactory";
+import { Transaction } from "../models/Transaction";
 
 @Injectable()
 export class MempoolService {
+  
+  private provider = getRPCProvider();
+  private mempool:TransactionIdentifier[] = [];  
 
-  public async getAllTransactions() {
-    const provider = getRPCProvider()    
+  constructor() {        
+
+    this.provider.on("pending", (tx) => {      
+      this.mempool.push(new TransactionIdentifier(tx.hash))      
+    })
     
-    return {
-      "transaction_identifiers": {}      
-    }
-
   }
 
-  public async getTransaction(transactionHash: string) {
-    const provider = getRPCProvider()    
+  public async getAllTransactions() {
+    return {
+      "transaction_identifiers": this.mempool      
+    }
+  }
+
+  public async getTransaction(transactionHash: string) {            
+    
+    const transaction = await this.provider.getTransaction(transactionHash)
+    const operationFactory = new OperationFactory()
+
+    const transfer = operationFactory.transferEWT(transaction.from, transaction.to, transaction.value, true);
     
     return {
-      "transaction_identifiers": {}
+      "tansaction": new Transaction(new TransactionIdentifier(transaction.hash), [
+      ...transfer])
     }
+  }
 
+  @Cron(CronExpression.EVERY_5_SECONDS)
+  mempoolWatchCron() {
+
+    this.mempool.forEach(tx => {
+      this.provider.getTransaction(tx.hash).then(transaction => {
+        if (transaction && transaction.blockNumber && transaction.confirmations > 0){          
+          this.mempool.splice(this.mempool.findIndex(x => x.hash === transaction.hash) , 1)
+        }
+        
+      })      
+    })
   }
 
 }
