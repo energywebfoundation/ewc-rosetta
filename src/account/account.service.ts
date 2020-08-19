004c412d4990eb4be655573f5c2f63ed8fd04ddd
Mani Hagh Sefat <mani.hagh.sefat@energyweb.org>
2020-08-19 11:52:27 +0800

    Add historical balance lookup

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,36 +1,50 @@
 import { Injectable } from "@nestjs/common";
+import { utils } from "ethers";
 import { getRPCProvider } from "../utils/client";
 import { Currency } from "../models/Currency";
-import { utils } from "ethers";
+import { Errors } from "../models/Errors";
 import { PartialBlockIdentifier } from "../models/PartialBlockIdentifier";
 
 @Injectable()
 export class AccountService {
 
-  public async getBalance(account: string, block?: number) {
+  public async getBalance(accountAddress: string, blockNumber?: number) {
     const provider = getRPCProvider()
     let blockIdentifier = new PartialBlockIdentifier()
     let balances = []
     
-    if (block){      
-      // historical balance lookup
-      // todo
-           
+    if (blockNumber){      
+      // historical balance lookup            
+      const block = await provider.getBlock(blockNumber)      
+      
+      if (!block || (block.number !== blockNumber)) {        
+        return Errors.BLOCK_NOT_FOUND;  
+      }
+      blockIdentifier.index = block.number
+      blockIdentifier.hash = block.hash
+
+      const balance = await provider.getBalance(accountAddress, blockIdentifier.index)      
+      balances.push({
+        value: utils.formatEther(balance),
+        currency: Currency.EWT,
+        metadata: {}
+      })
+
     } else {    
       // latest balance loookup
       const blockNumber = await provider.getBlockNumber()      
-      await provider.getBlock(blockNumber).then(block => {
-        blockIdentifier.index = block.number
-        blockIdentifier.hash = block.hash          
+      
+      const block = await provider.getBlock(blockNumber)
+      blockIdentifier.index = block.number
+      blockIdentifier.hash = block.hash
+
+      const balance = await provider.getBalance(accountAddress)                
+      balances.push({
+        value: utils.formatEther(balance),
+        currency: Currency.EWT,
+        metadata: {}
       })
-      await provider.getBalance(
-        account).then(balance => {          
-          balances.push({
-            value: utils.formatEther(balance),
-            currency: Currency.EWT,
-            metadata: {}
-          }) 
-      })
+
     }
 
     return {
