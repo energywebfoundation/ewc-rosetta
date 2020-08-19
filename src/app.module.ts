8edcbc28e24771db89cba69bbcefa8d97ac3663e
mroon <micha.roon@energyweb.org>
2020-08-20 00:13:45 +0200

    add NetworkModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { BlockModule } from "./block/block.module";
 import { AccountModule } from "./account/account.module";
+import { NetworkModule } from "./network/network.module";
 
 @Module({
-  imports: [BlockModule, AccountModule],
+  imports: [BlockModule, AccountModule, NetworkModule],
 })
 export class AppModule {}
