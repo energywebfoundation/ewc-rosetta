0ffd647037d2b701a926c68319ed7d3da7baeb57
Piotr Kosiński <piotrkosinski@gmail.com>
2020-08-20 14:12:20 +0200

    chore(): rosetta-api-cli config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from "@nestjs/common";
 import { BlockModule } from "./block/block.module";
-import { ConstructionModule } from './construction/construction.module';
+import { ConstructionModule } from "./construction/construction.module";
 import { AccountModule } from "./account/account.module";
 import { NetworkModule } from "./network/network.module";
 import { MempoolModule } from "./mempool/mempool.module";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { HTTPLoggingInterceptor } from "./HTTPLoggingInterceptor";
 
 @Module({
-  imports: [BlockModule, AccountModule, MempoolModule, ConstructionModule, NetworkModule],
+  imports: [
+    BlockModule,
+    AccountModule,
+    MempoolModule,
+    ConstructionModule,
+    NetworkModule,
+  ],
+  providers: [{ provide: APP_INTERCEPTOR, useClass: HTTPLoggingInterceptor }],
 })
 export class AppModule {}
