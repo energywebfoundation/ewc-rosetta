da7d14dd7ffe5156a738490fe173f248c9718856
Mani Hagh Sefat <mani.hagh.sefat@energyweb.org>
2020-08-20 13:48:25 +0800

    Enhance mempool service

--- src/mempool/mempool.service.ts
+++ src/mempool/mempool.service.ts
@@ -1,54 +1,78 @@
 import { Injectable } from "@nestjs/common";
 import { Cron, CronExpression } from "@nestjs/schedule";
+import { providers } from "ethers";
 import { getRPCProvider } from "../utils/client";
 import { TransactionIdentifier } from "../models/TransactionIdentifer";
 import { OperationFactory } from "../models/OperationFactory";
 import { Transaction } from "../models/Transaction";
+import { Errors } from "../models/Errors";
 
 @Injectable()
 export class MempoolService {
   
   private provider = getRPCProvider();
-  private mempool:TransactionIdentifier[] = [];  
+  private transactionCache: Map<string, providers.TransactionResponse> = new Map<string, providers.TransactionResponse>();
 
   constructor() {        
 
     this.provider.on("pending", (tx) => {      
-      this.mempool.push(new TransactionIdentifier(tx.hash))      
-    })
+      this.transactionCache.set(tx.hash, tx as providers.TransactionResponse)      
+    });
+
+    this.provider.on("block", (blockNumber) => {
+      this.provider.getBlockWithTransactions(blockNumber).then(block => {
+        block.transactions.forEach(transaction => {          
+          this.transactionCache.delete(transaction.hash)
+        });
+      });   
+    });
     
   }
 
-  public async getAllTransactions() {
+  public async getAllTransactions() {    
+  
+    let transactionIdentifiers: TransactionIdentifier[] = [];
+
+    [ ...this.transactionCache.keys() ].forEach(tx => {
+      transactionIdentifiers.push(new TransactionIdentifier(tx))
+    });
+
     return {
-      "transaction_identifiers": this.mempool      
+      "transaction_identifiers": transactionIdentifiers
     }
   }
 
-  public async getTransaction(transactionHash: string) {            
+
+  public async getTransaction(transactionHash: string) {                
     
-    const transaction = await this.provider.getTransaction(transactionHash)
-    const operationFactory = new OperationFactory()
+    if (!this.transactionCache.has(transactionHash)){
+      return Errors.TX_NOT_FOUND;
+    }
+    
+    const transaction = await this.provider.getTransaction(transactionHash);
+
+    const operationFactory = new OperationFactory();
 
     const transfer = operationFactory.transferEWT(transaction.from, transaction.to, transaction.value, true);
     
     return {
-      "tansaction": new Transaction(new TransactionIdentifier(transaction.hash), [
+      "transaction": new Transaction(new TransactionIdentifier(transaction.hash), [
       ...transfer])
     }
   }
 
-  @Cron(CronExpression.EVERY_5_SECONDS)
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
   mempoolWatchCron() {
 
-    this.mempool.forEach(tx => {
+    this.transactionCache.forEach(tx => {
       this.provider.getTransaction(tx.hash).then(transaction => {
+
         if (transaction && transaction.blockNumber && transaction.confirmations > 0){          
-          this.mempool.splice(this.mempool.findIndex(x => x.hash === transaction.hash) , 1)
-        }
-        
-      })      
-    })
+          this.transactionCache.delete(transaction.hash);
+        }        
+      });
+    });
   }
 
 }
