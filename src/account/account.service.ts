f6c0d26aa8e0a7257c7a5b990da83d467c9162d7
Piotr Kosiński <piotrkosinski@gmail.com>
2021-10-06 14:46:10 +0200

    chore: improve nodes querying reliability

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,22 +1,20 @@
-import { Injectable } from "@nestjs/common";
-import { getRPCProvider } from "../utils/client";
-import { Currency } from "../models/Currency";
-import { PartialBlockIdentifier } from "../models/PartialBlockIdentifier";
+import {Injectable} from "@nestjs/common";
+import {getRPCProvider} from "../utils/client";
+import {Currency} from "../models/Currency";
+import {PartialBlockIdentifier} from "../models/PartialBlockIdentifier";
+import {withRetry} from "../utils/withRetry";
 
 @Injectable()
 export class AccountService {
+    public async getBalance(accountAddress: string, blockNumber?: number) {
+        const provider = getRPCProvider()
 
-  public async getBalance(accountAddress: string, blockNumber?: number) {
-    const provider = getRPCProvider()
+        const block = await withRetry(() => provider.getBlock(blockNumber ?? "latest"));
+        const balance = await withRetry(() => provider.getBalance(accountAddress, block.number));
 
-    const block = await provider.getBlock(blockNumber ?? "latest");
-    const balance = await provider.getBalance(accountAddress, block.number);
-
-    return {
-      "block_identifier": new PartialBlockIdentifier(block.number, block.hash),
-      "balances": [{value: balance.toString(), currency: Currency.EWT}]
+        return {
+            "block_identifier": new PartialBlockIdentifier(block.number, block.hash),
+            "balances": [{value: balance.toString(), currency: Currency.EWT}]
+        }
     }
-
-  }
-
 }
