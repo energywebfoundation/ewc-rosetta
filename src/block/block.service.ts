69d213e3ec3e6d3efe18a471c26708cdecbaa318
Daniel Wojno <32939785+dwojno@users.noreply.github.com>
2022-04-20 13:12:35 +0200

    fix(block): rewrite parsing trace to operations

--- src/block/block.service.ts
+++ src/block/block.service.ts
@@ -1,183 +1,220 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { providers, BigNumber, utils } from "ethers";
+import { Injectable, Logger } from '@nestjs/common'
+import { providers, BigNumber, utils } from 'ethers'
 
-import { Block } from "../models/Block";
-import { OperationFactory } from "../models/OperationFactory";
-import { PartialBlockIdentifier } from "../models/PartialBlockIdentifier";
-import { Transaction } from "../models/Transaction";
-import { TransactionIdentifier } from "../models/TransactionIdentifer";
-import { getRPCProvider } from "../utils/client";
-import { RewardService } from "./reward.service";
-import { withRetry } from "../utils/withRetry";
-import { Operation } from "../models/Operation";
+import { Block } from '../models/Block'
+import { OperationFactory } from '../models/OperationFactory'
+import { PartialBlockIdentifier } from '../models/PartialBlockIdentifier'
+import { Transaction } from '../models/Transaction'
+import { TransactionIdentifier } from '../models/TransactionIdentifer'
+import { getRPCProvider } from '../utils/client'
+import { RewardService } from './reward.service'
+import { withRetry } from '../utils/withRetry'
+import { Operation } from '../models/Operation'
+import { CallType, EthTrace } from './block.types'
 
 @Injectable()
 export class BlockService {
-    private provider = getRPCProvider();
-    private readonly logger = new Logger(BlockService.name);
+  private provider = getRPCProvider()
+  private readonly logger = new Logger(BlockService.name)
 
-    constructor(private rewardService: RewardService) {
+  constructor(private rewardService: RewardService) {}
+
+  public async getBlock(block?: number) {
+    const {
+      transactions,
+      miner,
+      timestamp,
+      parentHash,
+      hash,
+      number: blockNumber,
+    } = await withRetry(() =>
+      this.provider.getBlockWithTransactions(block ?? 'latest')
+    )
+
+    const parent = await withRetry(() => this.provider.getBlock(parentHash))
+
+    const blockTransaction = await this.buildBlockTransactions(
+      miner,
+      transactions
+    )
+
+    const rewardTransaction = await this.getRewardTransaction(
+      blockNumber,
+      hash,
+      miner
+    )
+
+    return {
+      block: new Block(
+        new PartialBlockIdentifier(blockNumber, hash),
+        new PartialBlockIdentifier(
+          parent?.number ?? blockNumber,
+          parent?.hash ?? hash
+        ),
+        timestamp * 1000,
+        [...blockTransaction, rewardTransaction]
+      ),
+    }
+  }
+
+  public async getBlockTransaction(
+    blockNumber: number,
+    transactionHash: string
+  ) {
+    const transaction = await withRetry(() =>
+      this.provider.getTransaction(transactionHash)
+    )
+    if (transaction) {
+      if (transaction.blockNumber !== blockNumber) {
+        throw new Error('Blocknumber mismatch')
+      }
+
+      const block = await withRetry(() => this.provider.getBlock(blockNumber))
+
+      const blockTransaction = await this.buildBlockTransactions(block.miner, [
+        transaction,
+      ])
+
+      return {
+        transaction: blockTransaction,
+      }
     }
 
-    public async getBlock(block?: number) {
-        const {
-            transactions,
-            miner,
-            timestamp,
-            parentHash,
-            hash,
-            number: blockNumber,
-        } = await withRetry(() => this.provider.getBlockWithTransactions(block ?? 'latest'));
+    const block = await withRetry(() => this.provider.getBlock(transactionHash))
 
-        const parent = await withRetry(() => this.provider.getBlock(parentHash))
-
-        const blockTransaction = await this.buildBlockTransactions(
-            miner,
-            transactions
-        );
-
-        const rewardTransaction = await this.getRewardTransaction(
-            blockNumber,
-            hash,
-            miner
-        );
-
-        return {
-            block: new Block(
-                new PartialBlockIdentifier(blockNumber, hash),
-                new PartialBlockIdentifier(parent?.number ?? blockNumber, parent?.hash ?? hash),
-                timestamp * 1000,
-                [...blockTransaction, rewardTransaction]
-            ),
-        };
+    return {
+      transaction: await this.getRewardTransaction(
+        block.number,
+        block.hash,
+        block.miner
+      ),
     }
+  }
 
-    public async getBlockTransaction(
-        blockNumber: number,
-        transactionHash: string
-    ) {
-        const transaction = await withRetry(() => this.provider.getTransaction(transactionHash));
-        if (transaction) {
-            if (transaction.blockNumber !== blockNumber) {
-                throw new Error("Blocknumber mismatch");
-            }
+  private async getRewardTransaction(
+    blockNumber: number,
+    blockHash: string,
+    miner: string
+  ) {
+    const blockRewards = await this.rewardService.calculateBlockRewards(
+      miner,
+      blockNumber
+    )
 
-            const block = await withRetry(() => this.provider.getBlock(blockNumber));
+    const operationFactory = new OperationFactory()
 
-            const blockTransaction = await this.buildBlockTransactions(block.miner, [
-                transaction,
-            ]);
+    return new Transaction(
+      new TransactionIdentifier(blockHash),
+      blockRewards.flatMap(({ address, value }) =>
+        operationFactory.reward(address, value)
+      )
+    )
+  }
 
-            return {
-                transaction: blockTransaction,
-            };
-        }
+  private async buildBlockTransactions(
+    miner: string,
+    transactions: providers.TransactionResponse[]
+  ) {
+    const receipts = await Promise.all(
+      transactions.map((tx) =>
+        withRetry(() => this.provider.getTransactionReceipt(tx.hash))
+      )
+    )
 
-        const block = await withRetry(() => this.provider.getBlock(transactionHash));
+    const transactionCache = transactions.reduce(
+      (map, tx) => map.set(tx.hash, tx),
+      new Map<string, providers.TransactionResponse>()
+    )
 
-        return {
-            transaction: await this.getRewardTransaction(
-                block.number,
-                block.hash,
-                block.miner
-            ),
-        };
-    }
+    return Promise.all(
+      receipts.map(async (tx) => {
+        const operationFactory = new OperationFactory()
+        const { value, gasPrice, data } = transactionCache.get(
+          tx.transactionHash
+        )
+        const { gasUsed, status, from, to, contractAddress } = tx
+        const feeValue = gasPrice.mul(gasUsed)
+        const success = status === 1
 
-    private async getRewardTransaction(
-        blockNumber: number,
-        blockHash: string,
-        miner: string
-    ) {
-        const blockRewards = await this.rewardService.calculateBlockRewards(
-            miner,
-            blockNumber
-        );
+        let transfers = []
 
-        const operationFactory = new OperationFactory();
-
-        return new Transaction(
-            new TransactionIdentifier(blockHash),
-            blockRewards.flatMap(({ address, value }) =>
-                operationFactory.reward(address, value)
+        if (data != '0x') {
+          try {
+            const traces = await withRetry<EthTrace[]>(() =>
+              this.provider.send('trace_transaction', [tx.transactionHash])
             )
-        );
-    }
-
-    private async buildBlockTransactions(
-        miner: string,
-        transactions: providers.TransactionResponse[]
-    ) {
-        const receipts = await Promise.all(
-            transactions.map((tx) => withRetry(() => this.provider.getTransactionReceipt(tx.hash)))
-        );
-
-        const transactionCache = transactions.reduce(
-            (map, tx) => map.set(tx.hash, tx),
-            new Map<string, providers.TransactionResponse>()
-        );
-
-        return Promise.all(receipts.map(async (tx) => {
-            const operationFactory = new OperationFactory();
-            const { value, gasPrice, data } = transactionCache.get(tx.transactionHash);
-            const { gasUsed, status, from, to, contractAddress } = tx;
-            const feeValue = gasPrice.mul(gasUsed);
-            const success = status === 1;
-
-            let transfers = [];
-
-            if (data != '0x') {
-                try {
-                    const trace = await withRetry(() => this.provider.send('trace_transaction', [tx.transactionHash]));
-                    transfers = this.traceToTransfers(trace, operationFactory);
-                } catch (e) {
-                    this.logger.error(`Parsing trace output for tx ${tx.transactionHash} failed with error ${e.toString()}`);
-                }
-            } else {
-                transfers = operationFactory.transferEWT(
-                    from,
-                    to ?? contractAddress,
-                    value,
-                    success
-                );
-            }
-
-            const fee = operationFactory.fee(from, miner, feeValue);
-
-            return new Transaction(new TransactionIdentifier(tx.transactionHash), [
-                ...transfers,
-                ...fee,
-            ]);
+            transfers = this.traceToTransfers(traces, operationFactory, success)
+          } catch (e) {
+            this.logger.error(
+              `Parsing trace output for tx ${
+                tx.transactionHash
+              } failed with error ${e.toString()}`
+            )
+          }
+        } else {
+          transfers = operationFactory.transferEWT(
+            from,
+            to ?? contractAddress,
+            value,
+            success
+          )
         }
-        ))
+
+        const fee = operationFactory.fee(from, miner, feeValue)
+
+        return new Transaction(new TransactionIdentifier(tx.transactionHash), [
+          ...transfers,
+          ...fee,
+        ])
+      })
+    )
+  }
+
+  private traceToTransfers(
+    traces: EthTrace[],
+    operationFactory: OperationFactory,
+    success: boolean
+  ): Operation[] {
+    const operations: Operation[] = []
+
+    if (traces.length < 1) {
+      return []
     }
+    for (const trace of traces) {
+      const zeroValue = trace.action.value == '0x0'
+      const isCallType = CallType.has(trace.type.toUpperCase())
 
-    private traceToTransfers(trace: any, operationFactory: OperationFactory): Operation[] {
-        const internalTransfers = trace.filter(t => !t.error && (t.action.value != '0x0' || t.type === 'suicide'));
+      const shouldAddOperation = !(zeroValue && isCallType)
 
-        return internalTransfers.map(tx => {
-            let from, value, to;
+      if (shouldAddOperation) {
+        let from: string
+        let to: string
+        let value: string
 
-            if (tx.type === "create") {
-                from = tx.action.from;
-                to = tx.result.address;
-                value = tx.action.value;
-            } else if (tx.type == "suicide") {
-                from = tx.action.address;
-                to = tx.action.refundAddress;
-                value = tx.action.balance;
-            } else {
-                from = tx.action.from;
-                to = tx.action.to;
-                value = tx.action.value;
-            }
+        if (trace.type === 'create') {
+          from = trace.action.from;
+          to = trace.result.address;
+          value = trace.action.value;
+        } else if (trace.type == 'suicide') {
+          from = trace.action.address;
+          to = trace.action.refundAddress;
+          value = trace.action.balance;
+        } else {
+          from = trace.action.from;
+          to = trace.action.to;
+          value = trace.action.value;
+        }
 
-            return operationFactory.transferEWT(
-                utils.getAddress(from),
-                utils.getAddress(to),
-                BigNumber.from(value),
-                true
-            );
-        }).flat()
+        const newOperations = operationFactory.transferEWT(
+          utils.getAddress(from),
+          utils.getAddress(to),
+          BigNumber.from(value),
+          success
+        )
+
+        operations.push(...newOperations)
+      }
     }
+    return operations
+  }
 }
---
+++ src/block/block.types.ts
@@ -0,0 +1,34 @@
+interface EthTraceAction {
+  callType: string,
+  from: string,
+  gas: BigInt,
+  input: BigInt,
+  to: string,
+  value: string,
+  address: string,
+  balance: string,
+  refundAddress: string
+}
+
+interface EthTraceResult {
+  address: string,
+  gasUsed: string,
+  output: string,
+  code: string,
+}
+
+export interface EthTrace {
+  action: EthTraceAction,
+  result: EthTraceResult,
+  blockHash: string,
+  blockNumber: number,
+  error: string,
+  subtraces: number,
+  traceAddress: string[],
+  transactionHash: string,
+  transactionPosition: 0,
+  type: string
+}
+
+
+export const CallType = new Set(['CALL', 'CALLCODE', 'DELEGATECALL', 'STATICCALL'])
